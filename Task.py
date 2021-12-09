f38b476a7b5748e51feda2bd534737faee18cc23
19  5  2  1  19  20  9  1  14 <sebastianm880@deltalearns.ca>
2021-12-09 13:46:11 -0800

    Update Task.py

--- Task.py
+++ Task.py
@@ -3,7 +3,16 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 window = tk.Tk()
-window.title("Hi")
-window.geometry("50x100")
-
+window.title("tk")
+entry1 = tk.Entry(window, width = 30)
+Label2 = tk.Label(text ="x")
+entry2 = tk.Entry(window, width = 30)
+Label3 = tk.Label(text ="=")
+entry3 = tk.Entry(window, width = 30)
+window.geometry("450x25")
+entry1.pack(side=LEFT)
+Label2.pack(side=LEFT)
+entry2.pack(side=LEFT)
+Label3.pack(side=LEFT)
+entry3.pack(side=LEFT)
 window.mainloop()
