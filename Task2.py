b2288bec2ee8962eeb1277a217748645eab25aa3
19  5  2  1  19  20  9  1  14 <sebastianm880@deltalearns.ca>
2021-12-10 13:45:43 -0800

    sx

---
+++ Task2.py
@@ -0,0 +1,12 @@
+#! python3
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+window = tk.Tk()
+window.title("T-Town Veterinary Clinic Database")
+dogphoto = PhotoImage(file="dog.png")
+entry1 = tk.Entry(window, width = 20)
+entry2 = tk.Entry(window, width = 20)
+entry3 = tk.Entry(window, width = 20)
+entry4 = tk.Entry(window, width = 20)
+entry5 = tk.Entry(window, width = 20)
